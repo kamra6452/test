e824d8f1f9e72596e0bff0e4bcc7dcb22b536eb9
Sourav <Sourav.khunger@gmail.com>
2021-08-23 17:48:36 +0530

    First Commit

---
+++ updates-files.php
@@ -0,0 +1,12 @@
+1) http://ec2-18-195-213-147.eu-central-1.compute.amazonaws.com/admin/user/update/16 =====>>>>
+ /var/www/html/resources/views/pages/admin/users   update-users.blade.php
+ 
+ 2)http://ec2-18-195-213-147.eu-central-1.compute.amazonaws.com/admin/user/add
+ 
+ /var/www/html/resources/views/pages/admin/users      add-users.blade.php
+ 
+ 3) header-new.blade.php
+ 
+ 4) _FrontendManagerController.php
+ 
+ 5) 
